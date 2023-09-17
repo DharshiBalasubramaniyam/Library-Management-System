7e1c6db6b30b26a5a92d498f21458c8db8981587
Dharshib <Dharshib.8@gmail.com>
2023-09-17 07:56:58 +0530

    add books return functionalities

---
+++ Return/returnBook.php
@@ -0,0 +1,129 @@
+<?php
+    include("database.php");
+
+    $error = ""; $isbn = "";
+    $row = "";
+
+    if (isset($_POST["search"])) {
+        if (empty($_POST["isbn"])) {
+            $error = "Please enter the ISBN of the book!";
+        }
+        else {
+            $error = "";
+            $isbn = $_POST["isbn"];
+            $check = "SELECT * from borrow where isbn='$isbn' and returned_date is null";
+    
+            $result1 = mysqli_query($connection, $check);
+            $row = mysqli_fetch_assoc($result1);
+    
+            if (mysqli_num_rows($result1)==0) {
+                $error = "The book is not issued!";
+            }
+            else {
+            
+            }
+        }
+
+    }
+    $days = ""; $fine = "";
+    if (isset($_POST["return"])) {
+        $isbn = $_POST["isbn"];
+        $return = date('y-m-d', strtotime($_POST["returndate"]));
+        $fine = $_POST['fine'];
+
+        if (empty($_POST["return"]) || empty($_POST["fine"])) {
+            $error = "All fields are required!";
+        }
+        else {
+            $sql1 = "update book set status='Available' where isbn='$isbn'";
+            mysqli_query($connection, $sql1);
+            $sql2 = "update borrow set returned_date='$return', fine='$fine' where isbn='$isbn' and returned_date is null";
+            mysqli_query($connection, $sql2);
+            $error = "";
+            echo "<script> alert('The book successfully returned!') </script>";
+            $isbn = "";
+    }
+        }
+        
+    
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/style.css">
+    <title>Return Book</title>
+</head>
+<body>
+
+    <div class="container">
+        <div class="side-bar">
+        <div class="logo"><img src="images/logo.png" alt=""> <span>Library</span></div>
+            <ul>
+                <li><a href="dashboard.php">dashboard</a></li>
+                <li><a href="addBook.php">add books</a></li>
+                <li><a href="addMember.php">add members</a></li>
+                <li><a href="issueBook.php">issue books</a></li>
+                <li  class="active"><a href="returnBook.php">return books</a></li>
+                <li><a href="issuedBooks.php">View issued books</a></li>
+                <li><a href="returnedBooks.php">View returned books</a></li>
+                <li><a href="allBooks.php">view all books</a></li>
+                <li><a href="allMembers.php">view all members</a></li>
+                <li><a href="logout.php">log out</a></li>
+            </ul>
+        </div>
+        <div class="add return">
+            <form action="returnBook.php" method="post">
+                <h1>Return Books Details</h1>
+                <div class="error-msg"><?php echo "$error"; ?></div>
+                <div class="input-return-book">
+                    <input type="text" placeholder="Enter ISBN of Book" name="isbn" value="<?php echo "$isbn"; ?>">
+                    <input type="submit" value="Search" name="search">
+                </div>
+                <?php if ($row) { ?>
+                    
+                    <div class="hide-content">
+                        <table border="1px">
+                            <tr>
+                                <th>ISBN of Book</th>
+                                <th>Student ID</th>
+                                <th>Issue Date</th>
+                                <th>Due Date</th>
+                            </tr>
+                            <tr>
+                                <td><?php echo $row["isbn"] ?></td>
+                                <td><?php echo $row["member_id"] ?></td>
+                                <td><?php echo $row["issue_date"] ?></td>
+                                <td><?php echo $row["due_date"] ?></td>
+                            </tr>
+                        </table>
+                        <div class="input-box">
+                            <label for="duedate">Return Date: </label>
+                            <input type="date" id="returndate" name="returndate">
+                            <label for="fine">Fine(Rs.) : </label>
+                            <input type="text" id="fine" name="fine" value="0.0">
+                            <div class="btns">
+                                <input type="submit" value="Cancel" class="clear">
+                                <input type="submit" value="Return" name="return">
+                            </div>
+                        </div>
+                    </div>
+                <?php } ?>
+
+            </form>
+        </div>
+    </div>
+
+
+
+
+
+
+
+
+
+    <script src="script/script.js"></script>
+</body>
+</html>
---
+++ Return/returnedBooks.php
@@ -0,0 +1,103 @@
+<?php
+    include("database.php");
+
+    $key = "";
+
+    if (isset($_POST['search'])) {
+        $key = $_POST['key'];
+        $records = array();
+        $query = "SELECT * from borrow where returned_date is not null and (isbn='$key' or member_id='$key' or issue_date='$key' or due_date='$key')"; 
+        $result = mysqli_query($connection, $query);
+        if (mysqli_num_rows($result)!=0){
+            while ($row = mysqli_fetch_assoc($result)) {
+            $records[] = $row;
+            }
+        }
+    }
+    else {
+        $query = "SELECT * from borrow where returned_date is not null order by returned_date desc";
+        $result = mysqli_query($connection, $query);
+        $records = array();
+        if (mysqli_num_rows($result)!=0){
+            while ($row = mysqli_fetch_assoc($result)) {
+            $records[] = $row;
+            }
+        }
+    }
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/style.css">
+    <title>All books</title>
+</head>
+<body>
+
+    <div class="container">
+        <div class="side-bar">
+        <div class="logo"><img src="images/logo.png" alt=""> <span>Library</span></div>
+            <ul>
+                <li><a href="dashboard.php">dashboard</a></li>
+                <li><a href="addBook.php">add books</a></li>
+                <li><a href="addMember.php">add members</a></li>
+                <li><a href="issueBook.php">issue books</a></li>
+                <li><a href="returnBook.php">return books</a></li>
+                <li><a href="issuedBooks.php">View issued books</a></li>
+                <li  class="active"><a href="returnedBooks.php">View returned books</a></li>
+                <li><a href="allBooks.php">view all books</a></li>
+                <li><a href="allMembers.php">view all members</a></li>
+                <li><a href="logout.php">log out</a></li>  
+            </ul>
+        </div>
+        <div class="list">
+            <div class="content">
+                <h1>Returned Books List</h1>
+                <div class="top">
+                    <form action="returnedBooks.php" method="post" class="search-bar"> 
+                        <input type="text" placeholder="Search by student id, ISBN" name="key" value="<?php echo $key; ?>">
+                        <input type="submit" value="Search" name="search">
+                    </form>
+                </div>
+
+                <?php if(mysqli_num_rows($result)==0) {echo "<br> <br> <br>No records found!";} else { ?>
+                <div class="rows"><?php echo count($records) . " records found!<br><br>"; ?></div>
+                <div class="table">
+                <table>
+                    <tr>
+                        <th>ISBN</th>
+                        <th>Memder_Id</th>
+                        <th>Issue_date</th>
+                        <th>Due date</th>
+                        <th>Returned Date</th>
+                        <th>Fine</th>
+                    </tr>
+                    <?php foreach($records as $record) { ?>
+                        <tr>
+                            <td><?php echo $record["isbn"] ?></td>
+                            <td><?php echo $record["member_id"] ?></td>
+                            <td><?php echo $record["issue_date"] ?></td>
+                            <td><?php echo $record["due_date"] ?></td>
+                            <td><?php echo $record["returned_date"] ?></td>
+                            <td><?php echo $record["fine"] ?></td>
+                        </tr>
+                    <?php } ?>
+                </table>
+                </div>
+
+                <?php } ?>
+
+            </div>
+            
+            
+        </div>
+    </div>
+
+    <script src="script/script.js"></script>
+</body>
+</html>
